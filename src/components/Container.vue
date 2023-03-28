ca3da1d9caa509f458b53ebc1958b155fa0195ab
Hyekyung <mwjer333@gmail.com>
2023-03-29 00:18:16 +0900

    vuestagram push

---
+++ src/components/Container.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Post :뷰스타데이터="뷰스타데이터[i]" v-for="(a,i) in 뷰스타데이터" :key="i" />
+</div>
+</template>
+
+<script>
+import Post from './Post.vue';
+
+export default {
+    name: 'container',
+    props: {
+        뷰스타데이터: Array,
+    },
+    components: {
+        Post : Post
+    }
+
+}
+</script>
+
+<style>
+
+</style>
